fc3cac94a924cffa2421476a6930753400e85a43
WendelMinas <wendelleitedasilva@outlook.com>
2023-06-11 00:13:57 -0300

    atualizacao teste

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,10 +1,18 @@
 package application;
 
+import java.util.Scanner;
+
 public class Program {
 
 	public static void main(String[] args) {
 		
+		Scanner sc = new Scanner(System.in);
+		
 		System.out.println("Teste de git");
+		
+		
+		
+		sc.close();
 
 	}
 
