132276390a0a9df1fdfd272a29bc76a133ad5691
WendelMinas <wendelleitedasilva@outlook.com>
2023-06-11 22:30:19 -0300

    inclui formatação data e hora

--- src/entities/Matriz.java
+++ src/entities/Matriz.java
@@ -5,38 +5,37 @@ import java.util.Scanner;
 public class Matriz {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
 		int n = sc.nextInt();
 		int[][] mat = new int[n][n];
-		
-		for (int i=0; i<mat.length; i++) {
-			for (int j=0; j<mat[i].length; j++) {
-				mat[i][j] = sc.nextInt();				
-			}	
+
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
+				mat[i][j] = sc.nextInt();
+			}
 		}
 		System.out.println("Main diagonal: ");
-		for (int i=0; i<mat.length; i++){
-			System.out.print(mat[i][i] + " ");			
+		for (int i = 0; i < mat.length; i++) {
+			System.out.print(mat[i][i] + " ");
 		}
 		System.out.println();
-		
+
 		int count = 0;
-		for (int i=0; i<mat.length; i++) {
-			for (int j=0; j<mat[i].length; j++) {
+		for (int i = 0; i < mat.length; i++) {
+			for (int j = 0; j < mat[i].length; j++) {
 				if (mat[i][j] < 0) {
 					count++;
 				}
-				
-			}	
+
+			}
 		}
-		
+
 		System.out.println("Negative numbers = " + count);
-		
+
 		sc.close();
 
 	}
-		//https://github.com/acenelio/matrix1-java
+	// https://github.com/acenelio/matrix1-java
 }
-
